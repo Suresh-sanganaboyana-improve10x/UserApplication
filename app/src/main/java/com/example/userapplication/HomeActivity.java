f0729c350c5e4b93366b07d2b81c2cc12fd58dc3
Suresh <suresh.sanganaboyana@improve10x.com>
2023-06-17 12:36:03 +0530

    Implemented SplashActivity

--- app/src/main/java/com/example/userapplication/HomeActivity.java
+++ app/src/main/java/com/example/userapplication/HomeActivity.java
@@ -15,5 +15,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        getSupportActionBar().setTitle("Random Users");
     }
 }
--- app/src/main/java/com/example/userapplication/SplashActivity.java
+++ app/src/main/java/com/example/userapplication/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.userapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
     }
 }
